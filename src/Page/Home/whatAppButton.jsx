2ae82ecc6f2dafd0f6f90b5b85455c6085835e71
Hayder Mariem <adouni@Hayders-MacBook-Pro.local>
2025-05-17 12:14:28 -0700

    New

--- src/Page/Home/whatAppButton.jsx
+++ src/Page/Home/whatAppButton.jsx
@@ -5,7 +5,7 @@ import { FaWhatsapp } from 'react-icons/fa';
 const WhatsAppButton = () => {
   return (
     <a
-      href="https://wa.me/32470123456?text=Hello%20I%20am%20interested%20in%20your%20services"
+      href="https://wa.me/32485481543"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50 bg-green-500 p-4 rounded-full shadow-lg text-white hover:scale-110 transition duration-300"
